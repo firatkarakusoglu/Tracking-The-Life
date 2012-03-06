62e85d3a2f30658b3559f31fe1187e0773a9ef3d
Firat Karakusoglu <firatkarakusoglu@gmail.com>
2012-03-06 02:52:30 -0600

    First Commit - (firat)

---
+++ src/tracking/life/android/GoogleMap.java
@@ -0,0 +1,13 @@
+package tracking.life.android;
+
+public class GoogleMap {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main method - Google Map");
+	}
+
+}
